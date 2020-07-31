ef7590dd768a54015dc023f7f3b19e3b2bb50693
giselleLago <gisellelago25994@gmail.com>
2020-07-31 17:25:34 +0200

    Add project files.

---
+++ CW_TakeATenMinuteWalk/Kata.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_TakeATenMinuteWalk
+{
+    public class Kata
+    {
+        public bool IsValidWalk(string[] walk)
+        {
+            var countN = 0;
+            var countS = 0;
+            var countW = 0;
+            var countE = 0;
+            foreach (var item in walk)
+            {
+                switch (item)
+                {
+                    case "n":
+                        countN++;
+                        break;
+                    case "s":
+                        countS++;
+                        break;
+                    case "w":
+                        countW++;
+                        break;
+                    default:
+                        countE++;
+                        break;
+                }
+                
+            }
+            if (walk.Count() == 10 && (countN == countS && countW == countE))
+            {
+                return true;
+            }
+            return false;
+
+        }
+    }
+}
---
+++ CW_TakeATenMinuteWalk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_TakeATenMinuteWalk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.IsValidWalk(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }));
+            Console.ReadKey();
+        }
+    }
+}
